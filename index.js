5f4feeaf9099d2405c44bbf8fb2491e259188696
Ruben Cali <48370133+RubenCali@users.noreply.github.com>
2020-03-09 09:59:22 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+$("#h31").click(function() {
+  $("#naomi img").slideToggle(700);
+});
+$("#h32").click(function() {
+  $("#ghandi").fadeOut(1000);
+  $("#ghandi").fadeIn(1000);
+});
+    $("#h33").click(function(){
+      $("#einstein").animate({
+        height: 'toggle'
+      });
+    });
+        $("#h34").click(function(){
+          $("#rembrandt").css("color", "red").slideUp(2000).slideDown(2000);
+        });
